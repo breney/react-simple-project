31264276a13f0ed59149bdbb04f306c9bf6a2219
breney <brunopy2@gmail.com>
2021-05-06 11:46:01 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
 
 function App() {
+  const name = 'Breney' 
+  const x = false
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="container">
+      <h1>Hello From React</h1>
+      <h2>Hello {name}</h2>
+      <h3>Check : {x ? 'true' : 'false'}</h3>
+      <Header />
+    </div> 
+  )
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>Task</h1>
+        </header>
+    )
+}
+
+export default Header
